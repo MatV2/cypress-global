b69408102ada799a9978f951721704e7baf4b0bc
MatV2 <mathieu.v@myneolink.onmicrosoft.com>
2025-03-10 10:18:08 +0100

    1st commit

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+// cypress.config.js
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        }
+      })
+    },
+    // Activation des captures d'écran
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  // Configuration des captures d'écran
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  downloadsFolder: 'cypress/downloads',
+  fixturesFolder: 'cypress/fixtures',
+})
