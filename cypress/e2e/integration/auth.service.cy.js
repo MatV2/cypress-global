a6ae0c9528e0b197c763bb04fa7d815820353af3
MatV2 <mathieu.v@myneolink.onmicrosoft.com>
2025-03-12 16:26:20 +0100

    remove unused files

--- cypress/e2e/integration/auth.service.cy.js
+++
@@ -1,8 +0,0 @@
-import { authService } from '../../support/services/authService'
-
-describe('Authentication Service Tests', () => {
-  it('should login successfully', () => {
-    authService.login('user@example.com', 'password123')
-      .should('contain', 'Bienvenue')
-  })
-})
